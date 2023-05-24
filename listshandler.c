6613e40647ea1609594abb9288bb70af9b9a891e
John Mwadime <johnniekips@gmail.com>
2023-05-24 19:40:08 +0300

    Update listshandler.c

--- listshandler.c
+++ listshandler.c
@@ -1,161 +1,119 @@
 /*
- * File: handler.c
+ * File: listhandler.c
  * Auth: John Mwadime
  *       Lilian
  */
 
 #include "shell.h"
 
-void free_args(char **args, char **front);
-char *get_pid(void);
-char *get_env_value(char *beginning, int len);
-void variable_replacement(char **args, int *exe_ret);
+alias_t *add_alias_end(alias_t **head, char *name, char *value);
+void free_alias_list(alias_t *head);
+list_t *add_node_end(list_t **head, char *dir);
+void free_list(list_t *head);
 
 /**
- * free_args - Frees up memory taken by args.
- * @args: A null-terminated double pointer containing commands/arguments.
- * @front: A double pointer to the beginning of args.
+ * add_alias_end - Adds a node to the end of a alias_t linked list.
+ * @head: A pointer to the head of the list_t list.
+ * @name: The name of the new alias to be added.
+ * @value: The value of the new alias to be added.
+ *
+ * Return: If an error occurs - NULL.
+ *         Otherwise - a pointer to the new node.
  */
-void free_args(char **args, char **front)
+alias_t *add_alias_end(alias_t **head, char *name, char *value)
 {
-	size_t i;
+	alias_t *new_node = malloc(sizeof(alias_t));
+	alias_t *last;
 
-	for (i = 0; args[i] || args[i + 1]; i++)
-		free(args[i]);
+	if (!new_node)
+		return (NULL);
 
-	free(front);
+	new_node->next = NULL;
+	new_node->name = malloc(sizeof(char) * (_strlen(name) + 1));
+	if (!new_node->name)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	new_node->value = value;
+	_strcpy(new_node->name, name);
+
+	if (*head)
+	{
+		last = *head;
+		while (last->next != NULL)
+			last = last->next;
+		last->next = new_node;
+	}
+	else
+		*head = new_node;
+
+	return (new_node);
 }
 
 /**
- * get_pid - Gets the current process ID.
- * Description: Opens the stat file, a space-delimited file containing
- *              information about the current process. The PID is the
- *              first word in the file. The function reads the PID into
- *              a buffer and replace the space at the end with a \0 byte.
+ * add_node_end - Adds a node to the end of a list_t linked list.
+ * @head: A pointer to the head of the list_t list.
+ * @dir: The directory path for the new node to contain.
  *
- * Return: The current process ID or NULL on failure.
+ * Return: If an error occurs - NULL.
+ *         Otherwise - a pointer to the new node.
  */
-char *get_pid(void)
+list_t *add_node_end(list_t **head, char *dir)
 {
-	size_t i = 0;
-	char *buffer;
-	ssize_t file;
+	list_t *new_node = malloc(sizeof(list_t));
+	list_t *last;
 
-	file = open("/proc/self/stat", O_RDONLY);
-	if (file == -1)
-	{
-		perror("Cant read file");
+	if (!new_node)
 		return (NULL);
-	}
-	buffer = malloc(120);
-	if (!buffer)
-	{
-		close(file);
-		return (NULL);
-	}
-	read(file, buffer, 120);
-	while (buffer[i] != ' ')
-		i++;
-	buffer[i] = '\0';
 
-	close(file);
-	return (buffer);
+	new_node->dir = dir;
+	new_node->next = NULL;
+
+	if (*head)
+	{
+		last = *head;
+		while (last->next != NULL)
+			last = last->next;
+		last->next = new_node;
+	}
+	else
+		*head = new_node;
+
+	return (new_node);
 }
 
 /**
- * get_env_value - Gets the value corresponding to an environmental variable.
- * @beginning: The environmental variable to search for.
- * @len: The length of the environmental variable to search for.
- *
- * Return: If the variable is not found - an empty string.
- *         Otherwise - the value of the environmental variable.
- *
- * Description: Variables are stored in the format VARIABLE=VALUE.
+ * free_alias_list - Frees a alias_t linked list.
+ * @head: THe head of the alias_t list.
  */
-char *get_env_value(char *beginning, int len)
+void free_alias_list(alias_t *head)
 {
-	char **var_addr;
-	char *replacement = NULL, *temp, *var;
+	alias_t *next;
 
-	var = malloc(len + 1);
-	if (!var)
-		return (NULL);
-	var[0] = '\0';
-	_strncat(var, beginning, len);
-
-	var_addr = _getenv(var);
-	free(var);
-	if (var_addr)
+	while (head)
 	{
-		temp = *var_addr;
-		while (*temp != '=')
-			temp++;
-		temp++;
-		replacement = malloc(_strlen(temp) + 1);
-		if (replacement)
-			_strcpy(replacement, temp);
+		next = head->next;
+		free(head->name);
+		free(head->value);
+		free(head);
+		head = next;
 	}
-
-	return (replacement);
 }
 
 /**
- * variable_replacement - Handles variable replacement.
- * @line: A double pointer containing the command and arguments.
- * @exe_ret: A pointer to the return value of the last executed command.
- *
- * Description: Replaces $$ with the current PID, $? with the return value
- *              of the last executed program, and envrionmental variables
- *              preceded by $ with their corresponding value.
+ * free_list - Frees a list_t linked list.
+ * @head: The head of the list_t list.
  */
-void variable_replacement(char **line, int *exe_ret)
+void free_list(list_t *head)
 {
-	int j, k = 0, len;
-	char *replacement = NULL, *old_line = NULL, *new_line;
+	list_t *next;
 
-	old_line = *line;
-	for (j = 0; old_line[j]; j++)
+	while (head)
 	{
-		if (old_line[j] == '$' && old_line[j + 1] &&
-				old_line[j + 1] != ' ')
-		{
-			if (old_line[j + 1] == '$')
-			{
-				replacement = get_pid();
-				k = j + 2;
-			}
-			else if (old_line[j + 1] == '?')
-			{
-				replacement = _itoa(*exe_ret);
-				k = j + 2;
-			}
-			else if (old_line[j + 1])
-			{
-				/* extract the variable name to search for */
-				for (k = j + 1; old_line[k] &&
-						old_line[k] != '$' &&
-						old_line[k] != ' '; k++)
-					;
-				len = k - (j + 1);
-				replacement = get_env_value(&old_line[j + 1], len);
-			}
-			new_line = malloc(j + _strlen(replacement)
-					  + _strlen(&old_line[k]) + 1);
-			if (!line)
-				return;
-			new_line[0] = '\0';
-			_strncat(new_line, old_line, j);
-			if (replacement)
-			{
-				_strcat(new_line, replacement);
-				free(replacement);
-				replacement = NULL;
-			}
-			_strcat(new_line, &old_line[k]);
-			free(old_line);
-			*line = new_line;
-			old_line = new_line;
-			j = -1;
-		}
+		next = head->next;
+		free(head->dir);
+		free(head);
+		head = next;
 	}
 }
